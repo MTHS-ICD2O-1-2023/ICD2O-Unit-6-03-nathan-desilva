bac35b4508152fa6ab342eca3ecc6700f82fece5
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-06-16 19:37:21 +0000

    edited js and manifest

--- js/script.js
+++ js/script.js
@@ -21,19 +21,16 @@ const getWeather = async (URLAddress) => {
   try {
     const result = await fetch(URLAddress)
     const jsonData = await result.json()
-    console.log(jsonData)
-    // Get weather and convert from kelvin to celsius
-    const weather = Math.round(jsonData.main.temp - 273.15)
-    document.getElementById("api-weather").innerHTML = "<p>Temperature: " + weather + "°C</p>"
-    // Get weather icon
-    const icon = jsonData.weather[0].icon
-    const iconURL = "http://openweathermap.org/img/w/" + icon + ".png"
-    document.getElementById("api-icon").innerHTML = "<img src='" + iconURL + "' class='donut'></img>"
+    console.log(jsonData.main.temp)
+    const temperature = jsonData.main.temp - 273.15
+    const symbol = jsonData.weather[0].icon
+    const description = jsonData.weather[0].description
+    document.getElementById("api-weather").innerHTML = "The current weather is " + temperature.toFixed(0) + "°C" + "<img src='https://openweathermap.org/img/wn/" + symbol + "@2x.png' alt='Weather Icon' width='15%'>" + "with " + description
   } catch (err) {
     console.log(err)
+    document.getElementById("api-weather").innerHTML = "Error fetching current weather."
   }
 }
 
-getWeather(
-  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
-)
+
+getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
